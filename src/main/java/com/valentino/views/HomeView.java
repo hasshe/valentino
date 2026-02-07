4ce6895e09696c62004ce547ac21145dbc66ec07
hasshe <hassan.sheikha_97@outlook.com>
2026-02-08 00:18:27 +0100

    make button move when cursor comes close and play placeholder gif

--- src/main/java/com/valentino/views/HomeView.java
+++ src/main/java/com/valentino/views/HomeView.java
@@ -11,6 +11,8 @@ import com.vaadin.flow.router.Route;
 @Route("")
 public class HomeView extends VerticalLayout {
 
+    private Image popupGif;
+
     public HomeView() {
         addClassName("home-view");
         setSizeFull();
@@ -20,11 +22,22 @@ public class HomeView extends VerticalLayout {
         var image = new Image("hlg.gif", "Valentine GIF");
         image.setWidth("200px");
         
+        // Create a popup GIF that appears when button moves
+        popupGif = new Image("hlg.gif", "Nope!");
+        popupGif.setId("popup-gif");
+        popupGif.getStyle()
+            .set("position", "fixed")
+            .set("width", "150px")
+            .set("height", "150px")
+            .set("z-index", "10000")
+            .set("pointer-events", "none")
+            .set("display", "none");
+        
         var title = new H1("Will you be my Valentine? 💕");
         
         var layout = createButtonLayout();
 
-        add(image, title, layout);
+        add(image, title, layout, popupGif);
     }
 
     private HorizontalLayout createButtonLayout() {
@@ -33,6 +46,28 @@ public class HomeView extends VerticalLayout {
 
         yesButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         noButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+
+        noButton.getElement().executeJs(
+            "var btn = this;" +
+            "btn.addEventListener('mouseenter', function() {" +
+            "  var maxX = window.innerWidth - btn.offsetWidth - 20;" +
+            "  var maxY = window.innerHeight - btn.offsetHeight - 20;" +
+            "  var randomX = Math.floor(Math.random() * maxX);" +
+            "  var randomY = Math.floor(Math.random() * maxY);" +
+            "  btn.style.position = 'fixed';" +
+            "  btn.style.left = randomX + 'px';" +
+            "  btn.style.top = randomY + 'px';" +
+            "  btn.style.zIndex = '9999';" +
+            "  var gif = document.getElementById('popup-gif');" +
+            "  if (gif) {" +
+            "    gif.style.left = randomX + 'px';" +
+            "    gif.style.top = (randomY - 160) + 'px';" +
+            "    gif.style.display = 'block';" +
+            "    gif.src = 'hlg.gif?' + new Date().getTime();" +
+            "    setTimeout(function() { gif.style.display = 'none'; }, 1000);" +
+            "  }" +
+            "});"
+        );
 
         yesButton.addClickListener(event -> {
             yesButton.setText("Clicked!");
